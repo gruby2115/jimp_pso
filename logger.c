4b11a0a542da452060b3c4bfda4a11bfc6f93d55
Paweł Pietrzak <ppietrzak99@gmail.com>
2026-01-18 18:56:18 +0100

    Update logger.c

--- logger.c
+++ logger.c
@@ -1,28 +1,28 @@
 #include "logger.h"
 
-void init_log_file(const char* filename) {
-    FILE* f = fopen(filename, "w"); // Tryb "w" czyœci plik jeœli istnia³
+void init_log_file(const char* plik) {
+    FILE* f = fopen(plik, "w");
     if (f == NULL) {
-        perror("Nie udalo sie utworzyc pliku logow");
+        fprintf(stderr, "Nie udalo sie utworzyc pliku logow\n");
         return;
     }
 
-    // Nag³ówek CSV
+    // NagÂ³Ã³wek CSV
     fprintf(f, "iteration,particle_id,x,y,value\n");
 
     fclose(f);
 }
 
-void log_swarm_state(const char* filename, Swarm* swarm, int it) {
-    FILE* f = fopen(filename, "a"); // Tryb "a" dopisuje na koñcu pliku
+void log_swarm_state(const char* plik, Swarm* swarm, int it) {
+    FILE* f = fopen(plik, "a");
     if (f == NULL) {
-        return; // Jeœli nie mo¿na otworzyæ, trudno, nie przerywamy symulacji
+        return; // JeÅ“li nie moÂ¿na otworzyÃ¦, trudno, nie przerywamy symulacji
     }
 
     for (int i = 0; i < swarm->particle_count; i++) {
         Particle p = swarm->particles[i];
 
-        // Zapisujemy: numer iteracji, numer cz¹stki, x, y, wartoœæ sygna³u
+        // Zapisywanie: numeru iteracji, numeru czÂ¹stki, x, y, wartoÅ›ci sygnaÂ³u
         fprintf(f, "%d,%d,%.4f,%.4f,%.4f\n", it, i, p.x, p.y, p.current_val);
     }
 
