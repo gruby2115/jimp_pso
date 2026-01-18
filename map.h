129f4a8269e9e83d0dc135672885f8c598d9292c
Paweł Pietrzak <ppietrzak99@gmail.com>
2026-01-18 19:18:19 +0100

    Update map.h

--- map.h
+++ map.h
@@ -6,13 +6,14 @@
 
 // Struktura mapy
 typedef struct {
-    int width;
-    int height;
-    double** signal; // Tablica alokowana dynamicznie
+	int width;
+	int height;
+	double** signal; // Tablica sygnałów
 } Map;
 
-Map* load_map(char* file);
+Map* load_map(char* plik);
 double get_signal_value(Map* map, double x, double y);
 void free_map(Map* map);
+
 
 #endif
