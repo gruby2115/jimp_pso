b925fd343a60098e34a5fa5dd591845aea02ff51
Paweł Pietrzak <ppietrzak99@gmail.com>
2026-01-18 19:22:18 +0100

    Update map.c

--- map.c
+++ map.c
@@ -2,89 +2,89 @@
 
 #define KARA -10000.0
 
-Map* load_map(char* filename) {
-    FILE* file = fopen(filename, "r");
-    if (!file) {
-        fprintf(stderr, "Nie udalo sie otworzyc pliku mapy");
-        return NULL;
-    }
+Map* load_map(char* plik) {
+	FILE* file = fopen(filename, "r");
+	if (!file) {
+		fprintf(stderr, "Nie udalo sie otworzyc pliku mapy");
+		return NULL;
+	}
 
-    int w, h;
-    // Odczyt wymiarÛw
-    if (fscanf(file, "%d %d", &w, &h) != 2) {
-        fprintf(stderr, "Blad formatu naglowka mapy\n");
-        fclose(file);
-        return NULL;
-    }
+	int w, h;
+	// Odczyt wymiar√≥w
+	if (fscanf(file, "%d %d", &w, &h) != 2) {
+		fprintf(stderr, "Blad formatu naglowka mapy\n");
+		fclose(file);
+		return NULL;
+	}
 
-    // Alokacja struktury
-    Map* map = (Map*)malloc(sizeof(Map));
-    if (!map) {
-        fclose(file);
-        return NULL;
-    }
-    map->width = w;
-    map->height = h;
+	// Alokacja struktury
+	Map* map = (Map*)malloc(sizeof(Map));
+	if (!map) {
+		fclose(file);
+		return NULL;
+	}
+	map->width = w;
+	map->height = h;
 
-    // Dynamiczna alokacja tablicy sygna≥Ûw
-    map->signal = (double**)malloc(h * sizeof(double*));
-    if (!map->signal) {
-        free(map);
-        fclose(file);
-        return NULL;
-    }
+	// Dynamiczna alokacja tablicy sygna≈Ç√≥w
+	map->signal = (double**)malloc(h * sizeof(double*));
+	if (!map->signal) {
+		free(map);
+		fclose(file);
+		return NULL;
+	}
 
-    for (int i = 0; i < h; i++) {
-        map->signal[i] = (double*)malloc(w * sizeof(double));
-        if (!map->signal[i]) {
-            // W razie b≥Ídu trzeba zwolniÊ to co juø zaalokowaliúmy
-            for (int j = 0; j < i; j++)
-                free(map->signal[j]);
-            free(map->signal);
-            free(map);
-            fclose(file);
-            return NULL;
-        }
-    }
+	for (int i = 0; i < h; i++) {
+		map->signal[i] = (double*)malloc(w * sizeof(double));
+		if (!map->signal[i]) {
+			// W razie b≈Çƒôdu trzeba zwolniƒá to co ju≈º zaalokowali≈õmy
+			for (int j = 0; j < i; j++)
+				free(map->signal[j]);
+			free(map->signal);
+			free(map);
+			fclose(file);
+			return NULL;
+		}
+	}
 
-    // Wczytywanie wartoúci
-    for (int y = 0; y < h; y++) {
-        for (int x = 0; x < w; x++) {
-            if (fscanf(file, "%lf", &map->signal[y][x]) != 1) {
-                fprintf(stderr, "Blad odczytu danych mapy w wierszu %d\n", y);
-                fclose(file);
-                return NULL;
-            }
-        }
-    }
+	// Wczytywanie warto≈õci
+	for (int y = 0; y < h; y++) {
+		for (int x = 0; x < w; x++) {
+			if (fscanf(file, "%lf", &map->signal[y][x]) != 1) {
+				fprintf(stderr, "Blad odczytu danych mapy w wierszu %d\n", y);
+				fclose(file);
+				return NULL;
+			}
+		}
+	}
 
-    fclose(file);
-    return map;
+	fclose(file);
+	return map;
 }
 
 double get_signal_value(Map* map, double x, double y) {
-    // Rzutowanie pozycji (x, y) na indeksy ca≥kowite
-    int ix = (int)x;
-    int iy = (int)y;
+	// Rzutowanie pozycji (x, y) na indeksy ca≈Çkowite
+	int ix = (int)x;
+	int iy = (int)y;
 
-    // Obs≥uga wyjúcia poza zakres
-    if (ix < 0 || ix >= map->width || iy < 0 || iy >= map->height) {
-        return KARA;
-    }
+	// Obs≈Çuga wyj≈õcia poza zakres
+	if (ix < 0 || ix >= map->width || iy < 0 || iy >= map->height) {
+		return KARA;
+	}
 
-    return map->signal[iy][ix];
+	return map->signal[iy][ix];
 }
 
 void free_map(Map* map) {
-    if (map) {
-        if (map->signal) {
-            for (int i = 0; i < map->height; i++) {
-                if (map->signal[i]) {
-                    free(map->signal[i]);
-                }
-            }
-            free(map->signal);
-        }
-        free(map);
-    }
+	if (map) {
+		if (map->signal) {
+			for (int i = 0; i < map->height; i++) {
+				if (map->signal[i]) {
+					free(map->signal[i]);
+				}
+			}
+			free(map->signal);
+		}
+		free(map);
+	}
 }
